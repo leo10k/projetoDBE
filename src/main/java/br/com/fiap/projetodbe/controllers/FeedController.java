679270928e191cb0b818f86436ca8263fc44e20c
leo10k <leozinhomacedo41@gmail.com>
2023-04-10 23:30:01 -0300

    usando page e sort no metodo get

--- src/main/java/br/com/fiap/projetodbe/controllers/FeedController.java
+++ src/main/java/br/com/fiap/projetodbe/controllers/FeedController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.projetodbe.exception.RestNotFoundException;
@@ -28,17 +32,21 @@ public class FeedController {
     Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    FeedRepository repository; //IoD
+    FeedRepository feedRepository; //IoD
 
     @GetMapping
-    public List<Feed> index() {
-        return repository.findAll();
+    public Page<Feed> index(@RequestParam(required = false) String game, @PageableDefault(size = 5) Pageable pageble) {
+        if (game == null) 
+            return feedRepository.findAll(pageble);
+        
+        return feedRepository.findByGameContaining(game, pageble);
     }
+    
 
     @PostMapping
     public ResponseEntity<Feed> create(@RequestBody @Valid Feed feed) {
         log.info("cadastrando o feed: " + feed);
-        repository.save(feed);
+        feedRepository.save(feed);
         return ResponseEntity.status(HttpStatus.CREATED).body(feed);
     }
 
@@ -51,7 +59,7 @@ public class FeedController {
     @DeleteMapping("{id}")
     public ResponseEntity<Feed> destroy(@PathVariable Long id) {
         log.info("apagando feed com id " + id);
-        repository.delete(getFeed(id));  
+        feedRepository.delete(getFeed(id));  
         return ResponseEntity.noContent().build();
     }
 
@@ -60,12 +68,12 @@ public class FeedController {
         log.info("alterando feed com id " + id);
         getFeed(id);
         feed.setId(id);
-        repository.save(feed);
+        feedRepository.save(feed);
         return ResponseEntity.ok(feed);
     }
 
     private Feed getFeed(Long id) {
-        return repository.findById(id).orElseThrow(() -> new RestNotFoundException("feed nao encontrado"));
+        return feedRepository.findById(id).orElseThrow(() -> new RestNotFoundException("feed nao encontrado"));
     }
 
 }
--- src/main/java/br/com/fiap/projetodbe/repository/FeedRepository.java
+++ src/main/java/br/com/fiap/projetodbe/repository/FeedRepository.java
@@ -1,10 +1,14 @@
 package br.com.fiap.projetodbe.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.fiap.projetodbe.models.Feed;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+
+    Page<Feed> findByGameContaining(String game, Pageable pageble);
     
     //List<Feed> findByDate(LocalDate data);
 
