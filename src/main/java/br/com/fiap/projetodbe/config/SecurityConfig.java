a23b1f60baa9d284f799e6bed957b890790e1479
leo10k <leozinhomacedo41@gmail.com>
2023-08-21 23:25:49 -0300

    add a profile that will be better to develope

--- src/main/java/br/com/fiap/projetodbe/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/projetodbe/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.fiap.projetodbe.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -19,23 +20,33 @@ public class SecurityConfig {
     @Autowired
     AuthorizationFilter authorizationFilter;
 
+    @Autowired
+    Environment env;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http
-                .authorizeHttpRequests()
+        http
+            .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/users/login").permitAll()
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**" ).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .formLogin().disable()
-                .headers().frameOptions().sameOrigin()
-                .and()
-                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+            .and()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .formLogin().disable()
+            .headers().frameOptions().sameOrigin()
+            .and()
+            .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+            if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("open")) {
+                http.authorizeHttpRequests().anyRequest().permitAll();
+            } else {
+                http.authorizeHttpRequests().anyRequest().authenticated();
+            }
+
+            return http.build();
+
     }
 
     @Bean
